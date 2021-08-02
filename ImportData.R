1913aa23dd1fdadc147944c180f30a07b067a5d1
Nina <nina.gerber@uni-goettingen.de>
2021-08-02 15:24:44 +0200

    Add R code Import

---
+++ ImportData.R
@@ -0,0 +1,17 @@
+# clear global environment
+rm(list = ls())
+
+# load packages
+install.packages("dplyr")
+
+#get working directory
+
+getwd()
+
+#set working directory
+
+setwd()
+
+# read data
+
+read.csv(data,sep=";")
