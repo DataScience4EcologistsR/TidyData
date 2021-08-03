4929b6f7a54b3a6eebe9f81a704bb0a7595a20ef
Nina <nina.gerber@uni-goettingen.de>
2021-08-03 14:12:23 +0200

    Update ImportData.R

--- ImportData.R
+++ ImportData.R
@@ -1,18 +1,6 @@
 # clear global environment
 rm(list = ls())
 
-# install packages
-install.packages("dplyr")
-
-# more elegant way to load missing packages: 
-
-list.of.packages <- c("dplyr")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
-#load packages
-library(dplyr)
-
 #get working directory
 getwd()
 
@@ -20,7 +8,7 @@ getwd()
 setwd("~/Documents/GitHub/TidyData")
 
 # read data
-data<-read.csv("iris.csv")
+data<-read.csv("iris.csv", header=T,sep=",")
 
 #look at the structure of your data
 str(data)
@@ -28,3 +16,28 @@ str(data)
 #summarize your data
 summary(data)
 
+# change the class of a vector
+sex<- c(1,2,2,1,1,2,1,1)
+str(sex)
+
+
+sex<-as.factor(sex)
+str(sex)
+
+
+#get information in the R sesission
+#install.packages("devtools") # we'll get into installing and loading packages in the next video
+require(devtools) 
+session_info()
+
+# ─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────
+# setting  value                       
+# version  R version 3.6.2 (2019-12-12)
+# os       macOS Catalina 10.15.7      
+# system   x86_64, darwin15.6.0        
+# ui       RStudio                     
+# language (EN)                        
+# collate  de_CH.UTF-8                 
+# ctype    de_CH.UTF-8                 
+# tz       Europe/Zurich               
+# date     2021-08-03   
