546c389090f1021d1f9ae400b3d34d53c262b119
Nina <nina.gerber@uni-goettingen.de>
2021-08-03 09:22:41 +0200

    Update ImportData.R

--- ImportData.R
+++ ImportData.R
@@ -1,17 +1,27 @@
 # clear global environment
 rm(list = ls())
 
-# load packages
+# install packages
 install.packages("dplyr")
 
-#get working directory
+# more elegant way to load missing packages: 
 
+list.of.packages <- c("dplyr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#load packages
+library(dplyr)
+
+#get working directory
 getwd()
 
 #set working directory
-
-setwd()
+setwd("~/Documents/GitHub/TidyData")
 
 # read data
+data<-read.csv("iris.csv")
 
-read.csv(data,sep=";")
+#look at the structure of your data
+str(data)
+
