b882a78f775d6bb514b58cfae6873442c84bc2ff
Nina <nina.gerber@uni-goettingen.de>
2021-08-04 20:54:08 +0200

    Create structureData.R

---
+++ structureData.R
@@ -0,0 +1,56 @@
+# check the class of a variable
+someText<- c("This", "is", "some", "text")
+str(someText)
+
+
+# some other variables
+
+someNumber<- 5
+someOtherNumber <- 2
+
+str(someNumber)
+
+# This is numeric, this means we can do math with it! 
+
+someNumber + someOtherNumber
+
+# with ccharachter strings we can't do math
+
+someText * 2
+
+
+# also numbers can be non-numeric
+
+a<- "2"
+str(a)
+
+someNumber * a
+
+# but we can change the class of objects:
+
+a<- as.numeric(a)
+
+someNumber * a
+
+
+# objects that are numbers can also represent factors
+
+sites<- c(1,5,7,10)
+str(sites)
+
+sites<- as.factor(site)
+
+str(sites)
+
+#load the data
+data<-read.csv("iris.csv", header=T,sep=",")
+
+
+#look at the structure of your data
+str(data)
+
+#summarize your data
+summary(data)
+
+
+
