7ac2a37eca477bd02555ba7d147c566722074f81
Nina <nina.gerber@uni-goettingen.de>
2021-12-08 15:58:00 +0100

    small changes

--- 7_NA.R
+++ 7_NA.R
@@ -7,6 +7,11 @@ library(tidyverse)
 #load data
 dt<-read.csv("Output/all_sites_pivot_NA.csv", header=T,sep=",")
 
+
+# replace certain values  as  NA
+dt[dt=="na"]<-NA
+dt$site[dt$site=="0"]<-NA
+
 #check data
 head(dt)
 summary(dt)
@@ -14,3 +19,5 @@ summary(dt)
 is.na(dt)
 
 # fill() if e.g. species is  misisng
+
+
