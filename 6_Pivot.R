3a049f91a0b6040fbf7a3c4738875c4c15ca301b
Nina <nina.gerber@uni-goettingen.de>
2022-01-12 15:03:31 +0100

    add line without prefix

--- 6_Pivot.R
+++ 6_Pivot.R
@@ -9,29 +9,35 @@ dt<-read.csv("Output/all_sites_sep.csv", header=T,sep=",")
 
 #check data
 head(dt)
-str(dt)
 
 
 # what if a variable is spread across multiple columns? 
 
-dt2<- dt %>% pivot_longer(c(4,6,8),names_to = "year",values_to = "height", 
+dt_height<- dt%>% select(1:4,6,8)
+
+
+head(dt_height)
+
+dt_height_long <- dt_height %>% pivot_longer(c(4:6),names_to = "year",values_to = "height")
+
+dt_height_long <- dt_height %>% pivot_longer(c(4:6),names_to = "year",values_to = "height", 
                           names_prefix="height_year")
 
 # We could do this for each variable separately but we can also combine them
 
-dt3<-dt %>% pivot_longer(
+dt_long<-dt %>% pivot_longer(
   cols = 4:12,
   names_to = c( "measurment","year"), 
   names_pattern = "(.*)_year(.)",
   values_to = "value"
 )
 
-# in a second step make it wider again
+# But now we want our measurements as columns in a second step make it wider again
 
-dt4<-dt3 %>% pivot_wider(
+dt_tidy<-dt_long %>% pivot_wider(
   names_from = c( "measurment"), 
   values_from = "value"
 )
 
-write.csv(dt4,"Output/all_sites_pivot.csv" , row.names=F)
+write.csv(dt_tidy,"Output/all_sites_pivot.csv" , row.names=F)
 
